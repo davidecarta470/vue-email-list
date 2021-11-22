3097fa0b991fc7bb06a635d5db172306cdc52258
davidecarta470 <cartadavi@gmail.com>
2021-11-22 15:37:35 +0100

    first commit create list

---
+++ js/script.js
@@ -0,0 +1,25 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    mails:[],
+    isError:false
+  },
+
+  mounted(){
+    for(let i=0; i<10; i++){
+      axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+      .then((response)=>{
+        console.log(response)
+        const data= response.data;
+        console.log(data);
+        this.mails.push(data.response) ;
+         
+      })
+      
+      .catch((error)=>{
+        console.log(error)
+        this.isError=true
+      })
+    }
+  }
+})
